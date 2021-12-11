269dfc479a33ab29a87c5fe628d5c541f1d00158
DoanDuong99hust <doanduong1999@gmail.com>
2021-12-11 12:01:33 +0700

    add authorities to user

--- src/main/java/com/example/demosecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demosecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demosecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demosecurity.security.ApplicationUserPermission.*;
 import static com.example.demosecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -28,9 +30,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()// account with role_STUDENT can do all CRUD method
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                // account having COURSE_WRITE authority can do DELETE, PUT, POST methods
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                // account with roles ADMIN or ADMINTRAINEE can do GET method
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,19 +53,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails duongUser = User.builder()
                 .username("duong")
                 .password(passwordEncoder.encode("pass"))
-                .roles(STUDENT.name()) //ROLE_STUDENT
+//                .roles(STUDENT.name()) //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities()) // Add authority to STUDENT
                 .build();
 
         UserDetails shisuiUser = User.builder()
                 .username("shisui")
                 .password(passwordEncoder.encode("pass"))
-                .roles(ADMIN.name()) //ROLE_ADMIN
+//                .roles(ADMIN.name()) //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities()) // Add authority to ADMIN
                 .build();
 
         UserDetails doanUser = User.builder()
                 .username("doan")
                 .password(passwordEncoder.encode("pass"))
-                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+//                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities()) // Add authority to ADMINTRAINEE
                 .build();
         return new InMemoryUserDetailsManager(
                 duongUser,
